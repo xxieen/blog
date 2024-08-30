d6b2adbd6bf76777a520de31f63ce5c989fd6922
xieen <xieenenenen@outlook.com>
2024-08-31 00:15:25 +0800

    delete posts

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store","baby.jpg","favicon.png","post-images/.DS_Store","post-images/bear龙年新春.png","post-images/gcd.png","post-images/nQueue.webp","post-images/terminal-setup.jpg","post-images/tmux-setup.jpg","post-images/vim-essentials-cheatsheet.jpg","post-images/yabai-setup.jpg","profile-image.jpg"]),
 	mimeTypes: {".jpg":"image/jpeg",".png":"image/png",".webp":"image/webp"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.CuxSwhF6.js","app":"_app/immutable/entry/app.CsDBMw2k.js","imports":["_app/immutable/entry/start.CuxSwhF6.js","_app/immutable/chunks/entry.DqSf18z-.js","_app/immutable/chunks/scheduler.DKuV8lkR.js","_app/immutable/chunks/index.-TmmM991.js","_app/immutable/entry/app.CsDBMw2k.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js","_app/immutable/chunks/scheduler.DKuV8lkR.js","_app/immutable/chunks/index.BWbrWu7K.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.DRxuKV-_.js","app":"_app/immutable/entry/app.Dk_TpHrl.js","imports":["_app/immutable/entry/start.DRxuKV-_.js","_app/immutable/chunks/entry.CZt2lF8d.js","_app/immutable/chunks/scheduler.DKuV8lkR.js","_app/immutable/chunks/index.-TmmM991.js","_app/immutable/entry/app.Dk_TpHrl.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js","_app/immutable/chunks/scheduler.DKuV8lkR.js","_app/immutable/chunks/index.BWbrWu7K.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
---
+++ .vercel/output/static/_app/immutable/entry/start.DRxuKV-_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CZt2lF8d.js";export{t as start};
