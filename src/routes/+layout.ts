560072404685a59f399d1ddbdb5dd21c575595fb
xieen <xieenenenen@outlook.com>
2025-04-21 16:15:00 +0800

    turn off prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,7 +3,7 @@ import type { LayoutLoad } from "./$types";
 // add this to root layout
 // to prerender all our pages
 // during build time
-export const prerender = true;
+export const prerender = false;
 
 // get url path whenever visiting a new page
 export const load: LayoutLoad = ({ url }) => {
